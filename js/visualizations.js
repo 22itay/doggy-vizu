751816af6b2fd8dce8fedb242b6ef27a51e654f2
cN3rd <shlomo.shmouely@gmail.com>
2017-08-31 01:14:34 +0300

    second visualization extension

--- js/visualizations.js
+++ js/visualizations.js
@@ -68,7 +68,8 @@ function analyzeTests(error, subtests_descs, subtests_results) {
         .key(function (d) { return d.SubTestKind; })
         .key(function (d) { return d.Score; })
         .rollup(function (v) { return v.length; })
-        .entries(subtests_results);
+        .entries(subtests_results)
+
     summarized.forEach(function (entry) {
         entry.info = subtests[entry.key];
     })
@@ -110,7 +111,7 @@ d3.queue()
 
 // visual elements definition
 vis1 = d3.select("#vis1");
-vis2 = d3.select("#vis-tests").append("svg").attr("height", 1880);
+vis2 = d3.select("#vis-tests").append("svg").attr("class", "w-100");
 
 //// 
 window.addEventListener("dogDataLoaded", function () {
@@ -121,28 +122,52 @@ window.addEventListener("testDataLoaded", function () {
     visualizeTests();
 });
 
+function yIncreaser(step) {
+    counter = 0;
+    return () => step * counter++;
+}
+
+let wScale = d3.scale.linear();
+wScale.domain([0, d3.max(loadedData["subtests_summed"], d => d.values.reduce((sum, x) => sum + x.values, 0))]);
+wScale.range([0, 100]);
 
 function visualizeTests(filterStr = "") {
-    counter = 0;
-    getY = () => 40 * counter++;
+    let getY = yIncreaser(70);
+    let data = loadedData["subtests_summed"].filter(d => d.info.Description.includes(filterStr));
+    let colorScale = d3.scale.category10();
 
+    vis2.attr("height", data.length * 70);
     vis2.selectAll("g").remove();
 
     let dataElem = vis2.selectAll("g")
-        .data(loadedData["subtests_summed"].filter(function (d) {
-            return d.info.Description.includes(filterStr);
-        }));
+        .data(data);
 
-    let allGs = dataElem.enter()
+    dataElem.enter()
         .append("g")
-        .attr("transform", () => "translate(0," + getY() + ")");
-
-    allGs.append("text")
+        .attr("height", 80)
+        .attr("transform", () => "translate(0," + getY() + ")")
+        .append("text")
         .attr("x", "0")
         .attr("y", "20")
         .text(function (d) {
-            return `${d.info.Description}: ${Object.keys(d.values)}`
+            return `${d.info.Description}`
         });
+
+    dataElem.selectAll("g")
+        .data((d, i) => {
+            let sum = 0;
+            return d.values.map(k => {
+                k.cumsum = sum;
+                sum += k.values;
+                return k;
+            });
+        }).enter()
+        .append("rect")
+        .attr("height", 30)
+        .attr("width", (d, i, j) => { return wScale(d.values) + "%" })
+        .attr("y", 30)
+        .attr("x", (d, i, j) => { return wScale(d.cumsum) + "%" })
+        .attr("fill", (d, i) => colorScale(i)).attr("dx", 0)
 
     dataElem.exit()
         .remove();
