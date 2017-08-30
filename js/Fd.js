ee5f0ababce523d65e4ffd4dfdb3684786c49ead
22itay <22itay1998@walla.com>
2017-08-30 18:27:10 +0300

    add fix loc

--- js/Fd.js
+++ js/Fd.js
@@ -6,10 +6,24 @@ if (!window.d3) {
 var width=740, height=500;
 
 var linksdata = [
-    { source: '1', target:'2' },
-    { source: '1', target:'3' },
-    { source: '1', target:'4' },
-    { source: '2', target:'3' }
+    {"source":  0, "target":  1},
+    {"source":  1, "target":  2},
+    {"source":  2, "target":  0},
+    {"source":  1, "target":  3},
+    {"source":  3, "target":  2},
+    {"source":  3, "target":  4},
+    {"source":  4, "target":  5},
+    {"source":  5, "target":  6},
+    {"source":  5, "target":  7},
+    {"source":  6, "target":  7},
+    {"source":  6, "target":  8},
+    {"source":  7, "target":  8},
+    {"source":  9, "target":  4},
+    {"source":  9, "target": 11},
+    {"source":  9, "target": 10},
+    {"source": 10, "target": 11},
+    {"source": 11, "target": 12},
+    {"source": 12, "target": 10}
     ];
 
 
@@ -29,7 +43,8 @@ var linksdata = [
     .nodes(d3.values(nodes)) //add nodes
     .links(linksdata) //add links
     .on("tick", tick) //what to do
-    .linkDistance(300) //set for proper svg size
+    .linkDistance(35) //set for proper svg size
+    .charge(-400)
     .start(); 
 
     var svg=d3.select("#vis3").append('svg')
@@ -47,14 +62,16 @@ var linksdata = [
         .data(force.nodes()) //add
         .enter().append('circle')
         .attr('class', 'node')
-        .attr('r', width * 0.05); //radius of circle
+        .attr('r', width * 0.01)
+        .call(dragnode); //radius of circle
 
 
     function tick(e) {
         
         node.attr('cx', function(d) { return d.x; })
             .attr('cy', function(d) { return d.y; })
-            .call(force.drag);
+            .on("dblclick", dblclick);
+            //.call(force.drag);
             
         link.attr('x1', function(d) { return d.source.x; })
             .attr('y1', function(d) { return d.source.y; })
@@ -62,4 +79,15 @@ var linksdata = [
             .attr('y2', function(d) { return d.target.y; });
         
     }
+
+    var dragnode = force.drag()
+    .on("dragstart", dragstart);
+
+    function dblclick(d) {
+        d3.select(this).classed("fixed", d.fixed = false);
+      }
+      
+      function dragstart(d) {
+        d3.select(this).classed("fixed", d.fixed = true);
+      }
 
