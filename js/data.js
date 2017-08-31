c445152014b8ed5d0e07f76fa303792df1b5bbce
cN3rd <shlomo.shmouely@gmail.com>
2017-08-31 18:29:14 +0300

    organization & refactoring
    data processing split from visualization code + organized code

---
+++ js/data.js
@@ -0,0 +1,99 @@
+
+function toDate(dateStr, delimeter = '/') {
+    const [day, month, year] = dateStr.split(delimeter);
+    return new Date(+year, +month - 1, +day, 0, 0, 0, 0);
+}
+
+function analyzeDogs(err, data) {
+    dogs = {};
+
+    data.forEach(function (dogEntry) {
+        dogEntry.ID = +dogEntry.ID;
+        dogEntry.FatherID = +dogEntry.FatherID;
+        dogEntry.MotherID = +dogEntry.MotherID;
+        dogEntry["Age at Training"] = +dogEntry["Age at Training"];
+        dogEntry.Birthday = toDate(dogEntry.Birthday);
+        dogEntry.Passed = statuses.indexOf(dogEntry.Status) !== -1;
+        dogEntry.children = [];
+        dogEntry._children = [];
+        dogs[dogEntry.ID] = dogEntry;
+    });
+    loadedData["dogs"] = data;
+
+    dogTree = {};
+    data.forEach(function (dogEntry) {
+        let father_id = dogEntry.FatherID;
+        let mother_id = dogEntry.MotherID;
+        let father = dogs[father_id] || { "ID": father_id, "children": [] };
+        let mother = dogs[mother_id] || { "ID": mother_id, "children": [] };
+
+        if (!dogTree[father_id])
+            dogTree[father_id] = father;
+
+        if (!dogTree[mother_id]) {
+            dogTree[mother_id] = mother;
+        }
+
+        dogTree[father_id].children.push(dogEntry);
+        dogTree[mother_id].children.push(dogEntry);
+    });
+    console.log(dogTree);
+    console.log("------")
+    loadedData["dogsT"] = dogTree;
+    window.dispatchEvent(dogDataLoaded);
+}
+
+function analyzeTests(error, subtests_descs, subtests_results) {
+    subtests_results.forEach(function (resultEntry) {
+        resultEntry.SubTestID = +resultEntry.SubTestID;
+        resultEntry.TestID = +resultEntry.TestID;
+        resultEntry.SubTestKind = +resultEntry.SubTestKind;
+        resultEntry.Score = +resultEntry.Score;
+        resultEntry.Disqualified = +resultEntry.Disqualified === 1;
+    });
+
+    subtests = {}
+    subtests_descs.forEach(function (testEntry) {
+        testEntry.ID = +testEntry.ID;
+        testEntry.ColumnID = +testEntry.ColumnID;
+        subtests[testEntry.ID] = testEntry;
+    });
+
+    loadedData["subtests"] = subtests;
+    loadedData["subtest_results"] = subtests_results;
+    loadedData["subtests_descs"] = subtests_descs;
+
+    summed_scores = d3.nest()
+        .key(d => d.SubTestKind)
+        .key(d => d.Score)
+        .rollup(v => v.length)
+        .entries(subtests_results);
+
+    summed_disqualifications = d3.nest()
+        .key(d => d.SubTestKind)
+        .key(d => d.Disqualified)
+        .rollup(v => v.length)
+        .entries(subtests_results);
+
+    summed_scores.forEach(function (entry) {
+        entry.info = subtests[entry.key];
+    })
+
+    loadedData["subtests_summed_scores"] = summed_scores;
+
+    window.dispatchEvent(testDataLoaded);
+}
+
+let statuses = ["guiding"];
+var loadedData = {};
+let dogDataLoaded = new Event("dogDataLoaded");
+let testDataLoaded = new Event("testDataLoaded");
+
+// data loading queue
+d3.queue()
+    .defer(d3.csv, "/data/dogs.csv")
+    .await(analyzeDogs);
+d3.queue()
+    .defer(d3.csv, "/data/subtest-desc.csv")
+    .defer(d3.csv, "/data/subtests.csv")
+    .await(analyzeTests);
--- js/visualizations.js
+++ js/visualizations.js
@@ -1,94 +1,11 @@
-if (!window.d3) {
-    window.d3 = require("d3");
-    require("d3-queue");
+function stepper(step) {
+    counter = 0;
+    return () => step * counter++;
 }
 
-
-function toDate(dateStr, delimeter = '/') {
-    const [day, month, year] = dateStr.split(delimeter);
-    return new Date(+year, +month - 1, +day, 0, 0, 0, 0);
-}
-
-function analyzeDogs(err, data) {
-    dogs = {};
-
-    data.forEach(function (dogEntry) {
-        dogEntry.ID = +dogEntry.ID;
-        dogEntry.FatherID = +dogEntry.FatherID;
-        dogEntry.MotherID = +dogEntry.MotherID;
-        dogEntry["Age at Training"] = +dogEntry["Age at Training"];
-        dogEntry.Birthday = toDate(dogEntry.Birthday);
-        dogEntry.Passed = statuses.indexOf(dogEntry.Status) !== -1;
-        dogEntry.children = [];
-        dogEntry._children = [];
-        dogs[dogEntry.ID] = dogEntry;
-    });
-    loadedData["dogs"] = data;
-
-    dogTree = {};
-    data.forEach(function (dogEntry) {
-        let father_id = dogEntry.FatherID;
-        let mother_id = dogEntry.MotherID;
-        let father = dogs[father_id] || { "ID": father_id, "children": [] };
-        let mother = dogs[mother_id] || { "ID": mother_id, "children": [] };
-
-        if (!dogTree[father_id])
-            dogTree[father_id] = father;
-
-        if (!dogTree[mother_id]) {
-            dogTree[mother_id] = mother;
-        }
-
-        dogTree[father_id].children.push(dogEntry);
-        dogTree[mother_id].children.push(dogEntry);
-    });
-    console.log(dogTree);
-    console.log("------")
-    loadedData["dogsT"] = dogTree;
-    window.dispatchEvent(dogDataLoaded);
-}
-
-function analyzeTests(error, subtests_descs, subtests_results) {
-    subtests_results.forEach(function (resultEntry) {
-        resultEntry.SubTestID = +resultEntry.SubTestID;
-        resultEntry.TestID = +resultEntry.TestID;
-        resultEntry.SubTestKind = +resultEntry.SubTestKind;
-        resultEntry.Score = +resultEntry.Score;
-        resultEntry.Disqualified = +resultEntry.Disqualified === 1;
-    });
-
-    subtests = {}
-    subtests_descs.forEach(function (testEntry) {
-        testEntry.ID = +testEntry.ID;
-        testEntry.ColumnID = +testEntry.ColumnID;
-        subtests[testEntry.ID] = testEntry;
-    });
-
-    loadedData["subtests"] = subtests;
-    loadedData["subtest_results"] = subtests_results;
-    loadedData["subtests_descs"] = subtests_descs;
-
-    summed_scores = d3.nest()
-        .key(d => d.SubTestKind)
-        .key(d => d.Score)
-        .rollup(v => v.length)
-        .entries(subtests_results);
-
-    summed_disqualifications = d3.nest()
-        .key(d => d.SubTestKind)
-        .key(d => d.Disqualified)
-        .rollup(v => v.length)
-        .entries(subtests_results);
-
-    summed_scores.forEach(function (entry) {
-        entry.info = subtests[entry.key];
-    })
-
-    loadedData["subtests_summed_scores"] = summed_scores;
-
-    window.dispatchEvent(testDataLoaded);
-}
-
+/**
+ * Builds the first visualization
+ */
 function buildParsets(categories) {
     // recreate chart
     let sorted = Array.from(categories).sort();
@@ -103,76 +20,25 @@ function buildParsets(categories) {
     svg1.datum(loadedData["dogs"]).call(chart2);
 }
 
-/////
-
-let statuses = ["guiding"];
-var loadedData = {};
-let dogDataLoaded = new Event("dogDataLoaded");
-let testDataLoaded = new Event("testDataLoaded");
-
-// data loading queue
-d3.queue()
-    .defer(d3.csv, "/data/dogs.csv")
-    .await(analyzeDogs);
-d3.queue()
-    .defer(d3.csv, "/data/subtest-desc.csv")
-    .defer(d3.csv, "/data/subtests.csv")
-    .await(analyzeTests);
-
-// visual elements definition
-vis1 = d3.select("#vis1");
-vis2 = d3.select("#vis-tests").append("svg").attr("class", "w-100");
-vis2_legend = d3.select("#vis-tests-legend").append("svg").attr("width", "600").attr("height", "40");
-
-//// 
-window.addEventListener("dogDataLoaded", function () {
-    buildParsets(["Passed", "Breed & Color Code", "Gender"]);
-});
-
-window.addEventListener("testDataLoaded", function () {
-    let maxValue = d3.max(loadedData["subtests_summed_scores"], d => d.values.reduce((sum, x) => sum + x.values, 0));
-    wScale.domain([0, maxValue]);
-    wScale.range([0, 100]);
-
-    visualizeTests();
-    visualizeTestsScale();
-});
-
-function yIncreaser(step) {
-    counter = 0;
-    return () => step * counter++;
-}
-
-
-/// TOOLTIP
-var tooltip = d3.select("body")
-    .append("div")
-    .style("background", "rgba(255,255,255,1)")
-    .style("border", "1px solid #000")
-    .style("padding", "1rem")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
-    .text("a simple tooltip");
-
-let wScale = d3.scale.linear();
-let cScale = d3.scale.linear()
-    .domain([0, 100])
-    .range(["#FFFFDD", "#3E9583", "#1F2D86"].reverse())
-    .interpolate(d3.interpolateHcl);
-
+/**
+ * creates the Tests visualization.
+ * @param {string} filterStr String used for filtering
+ */
 function visualizeTests(filterStr = "") {
-    let getY = yIncreaser(70);
+    let getY = stepper(70);
     let data = loadedData["subtests_summed_scores"].filter(d => d.info.Description.includes(filterStr));
     let colorScale = d3.scale.category10();
 
     vis2.attr("height", data.length * 70);
     vis2.selectAll("g").remove();
 
+    // Remove old elements
     let dataElem = vis2.selectAll("g")
         .data(data);
 
-    dataElem.enter()
+    // Adds labels
+    // TODO: label toggle
+    let labels = dataElem.enter()
         .append("g")
         .attr("height", 80)
         .attr("transform", () => "translate(0," + getY() + ")")
@@ -183,7 +49,8 @@ function visualizeTests(filterStr = "") {
             return `${d.info.Description}`
         });
 
-    dataElem.selectAll("g")
+    // Adds stacked bar charts
+    let stackedBars = dataElem.selectAll("g")
         .data((d, i) => {
             let sum = 0;
             return d.values.map(k => {
@@ -198,7 +65,10 @@ function visualizeTests(filterStr = "") {
         .attr("width", (d, i, j) => { return wScale(d.values) + "%" })
         .attr("y", 30)
         .attr("x", (d, i, j) => { return wScale(d.cumsum) + "%" })
-        .attr("fill", (d, i) => cScale(wScale(d.values)))
+        .attr("fill", (d, i) => cScale(wScale(d.values)));
+
+    // Add tooltip events
+    stackedBars
         .on("mouseover", function (d) {
             tooltip
                 .html(`Option: "${d.key}"<br>Votes: ${d.values} (${wScale(d.values).toFixed(3)}%)`)
@@ -218,6 +88,9 @@ function visualizeTests(filterStr = "") {
         .remove();
 }
 
+/**
+ * Creates the scale
+ */
 function visualizeTestsScale() {
     let axisScale = d3.scale.linear()
         .domain([0, 100])
@@ -251,3 +124,41 @@ function visualizeTestsScale() {
         .call(d3.svg.axis()
             .scale(axisScale).tickFormat(d => d + "%"));
 }
+
+// visual elements definition
+let vis1 = d3.select("#vis1");
+let vis2 = d3.select("#vis-tests").append("svg").attr("class", "w-100");
+let vis2_legend = d3.select("#vis-tests-legend").append("svg").attr("width", "600").attr("height", "40");
+
+// event listeners
+window.addEventListener("dogDataLoaded", function () {
+    buildParsets(["Passed", "Breed & Color Code", "Gender"]);
+});
+
+window.addEventListener("testDataLoaded", function () {
+    let maxValue = d3.max(loadedData["subtests_summed_scores"], d => d.values.reduce((sum, x) => sum + x.values, 0));
+    wScale.domain([0, maxValue]);
+    wScale.range([0, 100]);
+
+    // call the visualizations
+    visualizeTests();
+    visualizeTestsScale();
+});
+
+/// TOOLTIP
+let tooltip = d3.select("body")
+    .append("div")
+    .style("background", "rgba(255,255,255,1)")
+    .style("border", "1px solid #000")
+    .style("padding", "1rem")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .text("a simple tooltip");
+
+
+let wScale = d3.scale.linear();
+let cScale = d3.scale.linear()
+    .domain([0, 100])
+    .range(["#FFFFDD", "#3E9583", "#1F2D86"].reverse())
+    .interpolate(d3.interpolateHcl);
